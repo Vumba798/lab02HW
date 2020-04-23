2cce585fa03f510be7014b137568ddbfb1957f43
Vumba798 <alexandrov32649@gmail.com>
2020-04-23 13:08:52 +0300

    changed hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(int argc, char* argv){
-	string name;
-	cout << "Input user_name":
-	cin >> name;
-	cout << "Hello world from " << name << endl;
-	return 0;
+string name;
+cout << "Input user_name":
+cin >> name;
+cout << "Hello world from " << name << endl;
+return 0;
 }
