c820489ed72003caa1520c223895fea1b88a08b9
Vumba798 <alexandrov32649@gmail.com>
2020-04-23 13:22:51 +0300

    fixed using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main(int argc, char* argv){
-string name;
-cout << "Input user_name":
-cin >> name;
-cout << "Hello world from " << name << endl;
+std::string name;
+std::cout << "Input user_name":
+std::cin >> name;
+std::cout << "Hello world from " << name << std::endl;
 return 0;
 }
