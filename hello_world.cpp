c0d9a7225312c3b0696f9a885f095b490e432440
Vumba798 <alexandrov32649@gmail.com>
2020-04-23 13:44:16 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main(int argc, char* argv){
-std::string name;
+std::string name; // используем переменную типа string
 std::cout << "Input user_name":
-std::cin >> name;
-std::cout << "Hello world from " << name << std::endl;
+std::cin >> name; // вводим своё имя в переменную
+std::cout << "Hello world from " << name << std::endl; // выводим приветствие
 return 0;
 }
