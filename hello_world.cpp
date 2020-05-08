8ccd6c19806b2c150ef5d65d92748c09d4e5f1bc
Vumba798 <61025343+Vumba798@users.noreply.github.com>
2020-05-08 16:36:46 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main(int argc, char* argv){
-std::string name; // используем переменную типа string
+std::string name; // uses string variable
 std::cout << "Input user_name":
-std::cin >> name; // вводим своё имя в переменную
-std::cout << "Hello world from " << name << std::endl; // выводим приветствие
+std::cin >> name; // reads your name
+std::cout << "Hello world from " << name << std::endl; // prints greetings
 return 0;
 }
